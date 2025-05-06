6c2b6fcc9c495a3e5cb6874c15f34dbf350fe32f
haythem-farjallah <demonswolfyy@yahoo.com>
2025-05-06 21:04:13 +0100

    add test to test

--- frontend/cypress/e2e/smoke.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Smoke-test', () => {
-    it('opens the Vite app', () => {
-      cy.visit('/')
-      cy.contains('body', 'Vite').should('be.visible')   // tweak wording to match your page
-    })
-  })
-  
